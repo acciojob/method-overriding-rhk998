5d440ed5dce8e5b4f34dc4f90aa000ce53e65d42
rhk998 <hemanthk.998r@gmail.com>
2024-04-11 17:54:17 +0530

    assg-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        System.out.println(objB.meth());
+        System.out.println(objB.meth());
+
+    }
+}
+class B extends A {
+    String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
+class A {
+    String meth(){
+        return "Invoking method from class A";
+    }
 }
